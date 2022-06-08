bfc0ec3d4fe9ae7a7adb471d6905e0031074b4b7
AlexPap <arpappagallo@gmail.com>
2022-06-08 19:18:09 -0400

    model data

---
+++ models/Pet.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Post model
+class Pet extends Model {}
+
+// create fields/columns for Post model
+Pet.init(
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        petname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pet_info: {
+            //leaving as text until cloudinary images are setup
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id'
+            }
+        }
+    }
+    );
+
+module.exports = Pet;
