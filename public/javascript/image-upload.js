4164cfc4fbf57173d0c7dd437ad45ec7b391d087
Jeffrey Young <jeffreymiyoung@aol.com>
2022-06-13 19:08:10 -0400

    added basic image-upload.js and handlebars for testing

--- public/javascript/image-upload.js
+++ public/javascript/image-upload.js
@@ -1,1 +1,22 @@
 // Image Upload Logic
+window.addEventListener("load", function () {
+    const form = document.getElementById("upload");
+  
+    function uploadImage(e) {
+      e.preventDefault();
+      const formData = new FormData(upload);
+      for (let [name, value] of formData.entries()) {
+        console.log(name, value);
+      }
+      fetch("/api/image", {
+        method: "POST",
+        body: formData,
+      }).then((res) => {
+        if (res.ok) {
+          console.log("Complete!");
+        }
+      });
+    }
+  
+    form.addEventListener("submit", uploadImage);
+});
